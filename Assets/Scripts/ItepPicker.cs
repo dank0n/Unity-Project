c5c1d51fcb9c8d38c314c56190844e363696db48
Danylo Kondratiuk <55409821+dank0n@users.noreply.github.com>
2020-11-25 23:22:12 +0200

    Lab1

---
+++ Assets/Scripts/ItepPicker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItepPicker : MonoBehaviour
+{
+    [SerializeField] private int _healValue;
+    [SerializeField] private int _mpValue;
+    [SerializeField] private bool _triplejump;
+    private void OnTriggerEnter2D(Collider2D info)
+    {
+        info.GetComponent<Player_Controller>().ChangeHp(_healValue);
+        info.GetComponent<Player_Controller>().ChangeMP(_mpValue);
+        if(info.GetComponent<Movement_Controller>()._morejumps == false)
+        {   
+            info.GetComponent<Movement_Controller>().MoreJumps(_triplejump);
+        }
+        Destroy(gameObject);
+    }
+}
